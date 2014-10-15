96e92e2231fe6491b26cccf81910915c633e76ae
dvdrtrgn <dvdrtrgn@gmail.com>
2014-10-15 00:50:48 -0500

    seek ways to automate

--- tasks/default.js
+++ tasks/default.js
@@ -18,6 +18,36 @@ module.exports = function (grunt) {
         'watch',
     ]);
 
+    grunt.registerMultiTask('wrap', 'Wraps source files with specified header and footer', function () {
+        var data = this.data,
+            path = require('path'),
+            dest = grunt.template.process(data.dest),
+            files = grunt.file.expandFiles(this.file.src),
+            header = grunt.file.read(grunt.template.process(data.header)),
+            footer = grunt.file.read(grunt.template.process(data.footer)),
+            sep = grunt.utils.linefeed;
+
+        files.forEach(function (f) {
+            var p = dest + '/' + path.basename(f),
+                contents = grunt.file.read(f);
+
+            grunt.file.write(p, header + sep + contents + sep + footer);
+            grunt.log.writeln('File "' + p + '" created.');
+        });
+    });
+
+    grunt.foo = (function () {
+        var out = {};
+        grunt.file.expand({
+            cwd: 'libs',
+            filter: 'isDirectory'
+        }, '*').map(function (nom) {
+            out['app/build/' + nom + '.js'] = ['libs/' + nom + '/*.js'];
+        });
+        console.log('GRUNT.FOO', out);
+        return out;
+    }());
+
     // grunt.registerTask('custom', 'Say hello!', function () {
     //     grunt.log.writeln("Custom task log");
     // });

2439bc143d0e93763d7dc3542f24139033b7d874
dvdrtrgn <dvdrtrgn@gmail.com>
2014-10-16 01:33:29 -0500

    more gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,54 @@
-var gulp   = require('gulp'),
-    gutil  = require('gulp-util'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    jshint = require('gulp-jshint'),
-    sass   = require('gulp-sass'),
-    reload = require('gulp-livereload');
-
+/*jslint es5:true, white:false */
+/*globals console, require, */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// http://gulpjs.com/
+var pkg    = require('./package.json');
+var gulp   = require('gulp');
+var gutil  = require('gulp-util');
+var concat = require('gulp-concat');
+var header = require('gulp-header');
+var jshint = require('gulp-jshint');
+var reload = require('gulp-livereload');
+var sass   = require('gulp-sass');
+var serve  = require('gulp-serve');
+var uglify = require('gulp-uglify');
+var info   = '// <%= pkg.name %>@v<%= pkg.version %>, <%= pkg.repository.url %>\n';
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+(function (x) {
+    var i, a = [];
+    for (i in x) {
+        a.push([i, x[i]]);
+    }
+    console.log(a);
+}(gulp));
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 gulp.task('js', function () {
     gulp.src('scripts/*.js') //
     .pipe(uglify()) //
+    .pipe(header(info, { pkg : pkg }))
     .pipe(concat('all.js')) //
     .pipe(gulp.dest('app/build'));
 });
 
-gulp.task('default', function () {
-    gulp.watch('scripts/*.js', ['js'])
+gulp.task('watch', function () {
+    reload.listen();
+    gulp.watch('build/**').on('change', reload.changed);
+    gulp.watch('scripts/*.js', ['js']);
 });
+
+gulp.task('blab', function () {
+    gulp.watch(['***'], function (event) {
+        console.log(['File', event.path, 'was', event.type].join(' '));
+    });
+});
+
+gulp.task('serve', function () {
+    serve({
+        root: pkg.bases,
+        port: pkg.port0,
+        middleware: function (req, res) {},
+    });
+    gulp.watch('build/**').on('change', reload.changed);
+});
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+gulp.task('default', function () {});

cfa8d7b2ac50476b302b07759ded645c483ce1f9
dvdrtrgn <dvdrtrgn@gmail.com>
2015-06-18 16:26:18 -0500

    tweak for msie

--- app/mvc/js/Controllers/AddController.js
+++ app/mvc/js/Controllers/AddController.js
@@ -6,7 +6,9 @@ define(['Views/AddView', 'Models/User'], function(AddView, User){
     }
 
     function bindEvents(){
-        document.getElementById('add').addEventListener('click', function(){
+        var ok = '\v'!='v';
+
+        document.getElementById('add')[ok?'addEventListener':'attachEvent'](ok?'click':'onclick', function(){
             var users = JSON.parse(localStorage.users);
             var userName = document.getElementById('user-name').value;
             users.push(new User(userName));

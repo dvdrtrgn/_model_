2db2ddfe145c1a6cab00708745b5791d0a542932
dvdrtrgn <dvdrtrgn@gmail.com>
2016-01-15 16:16:24 -0600

    wrap slide

---
+++ app/tests/slide/_main.js
@@ -0,0 +1,69 @@
+/*jslint white:false */
+/*global define, window, Projector */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ created drt 2014
+
+ USE
+ hook up various sub systems
+ bind events
+ store configs
+
+ TODO
+ document a bit
+ modernize api
+ loosely load
+ change 'index' data to 'eq'
+ */
+require(['../../config'], function () {
+    var W = (W && W.window || window),
+        C = (W.C || W.console || {});
+
+    require.config({
+        baseUrl: '../..',
+        deps: ['../../config'],
+    });
+
+    require(['lodash', 'iscroll', 'lib/scroller', 'lib/projector'], function (_, IScroll) {
+        'use strict';
+
+        var Nom = 'Main',
+            Df;
+
+        Df = { // DEFAULTS
+            projector: null,
+        };
+
+        function _init() {
+            C.info(Nom, 'init @ ' + Date(), {
+                debug: W.debug,
+            });
+
+            $(document).on('touchmove', function (e) {
+                e.preventDefault();
+            }, false);
+
+            function nametag(sel) {
+                // http://iscrolljs.com/#configuring
+                sel = $(sel || '.stars div');
+                // add fixed center cite?
+                sel.each(function () {
+                    var me = $(this), tag;
+
+                    tag = $('<cite>').addClass('marker') //
+                        .text(me.text()).wrapInner('<span>');
+                    me.text('');
+                    // add corner cites
+                    'tl tr br bl md'.split(' ').forEach(function (x) {
+                        me.append(tag.clone().addClass(x));
+                    });
+                });
+            }
+            Df.projector = Projector.attach('.iS-port');
+        }
+        _.delay(_init, 333);
+    });
+});
+/*
+
+
+ */

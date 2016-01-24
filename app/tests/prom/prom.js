0e3b5869bdcd15c94a3c1665758c3885d8e79add
dvdrtrgn <dvdrtrgn@gmail.com>
2016-01-23 20:59:35 -0600

    refine format and output

--- app/tests/prom/prom.js
+++ app/tests/prom/prom.js
@@ -6,15 +6,26 @@ function perchance() {
 function chance(num) {
     return Boolean(perchance() <= (num || 50));
 }
+function relay(fn, sec) {
+    var cb = fn;
+
+    if (sec === (sec | 0)) {
+        sec -= 1;
+        cb = function () {
+            console.info('relay', sec + 1);
+            fn();
+        };
+    }
+    return setTimeout(cb, sec * 1e3);
+}
 function mock(fn) {
-    return setTimeout(fn, Math.random() * 999);
+    return relay(fn, Math.random());
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /// BASICS
 
 function fn1() {
-
     var task = new Promise(function (res, rej) {
         // Do an async task async task and then...
         mock(function () {
@@ -26,22 +37,21 @@ function fn1() {
         });
     });
 
-    task.then(function (a) {
-        console.log('fn1 //', [a], 'Do something with the result :)');
-    }).catch(function (a) {
-        console.log('fn1 //', [a], 'Coin flipped...shit happens :(');
+    task.then(function (rez) {
+        console.log('fn1 //', [rez], 'Do something with the result :)');
+    }).catch(function (err) {
+        console.warn('fn1 //', [err], 'Coin flipped...shit happens :(');
     });
 }
 // It's up to the developer to manually call resolve or reject within
 // the body of the callback based on the result of their given task.
 function fn2() {
-
     // Return a new promise.
     function get(url) {
         return new Promise(function (res, rej) {
-
             var req = new XMLHttpRequest(); // Do the usual XHR stuff
-            req.open('GET', url);
+
+            req.open('get', url);
 
             req.onload = function () {
                 // This is called even on 404 etc so check the status
@@ -56,21 +66,20 @@ function fn2() {
             req.onerror = function () {
                 rej(Error('Network Error'));
             };
-            // Make the request
-            req.send();
+
+            req.send(); // Make the request
         });
     }
 
     // Use it!
     get('data/story.json').then(function (response) {
-        console.log('fn2 // Success!', response);
+        console.log('fn2 // Success!', JSON.parse(response));
     }, function (error) {
-        console.error('fn2 // Failed!', error);
+        console.warn('fn2 // Failed!', error);
     });
 }
 // Sometimes you don't need to complete an async tasks within the promise
 function fn3() {
-
     var userCache = {
         bob0: 'Roberts'
     };
@@ -85,14 +94,13 @@ function fn3() {
 
         // Use the fetch API, it returns a promise
         return fetch('data/' + username + '.json')
-            .then(function(response) {
+            .then(function (response) {
                 return response.json(); // Convert to JSON
             }).then(function (result) {
                 userCache[username] = result.lastName;
                 console.log('fn3 // userCache', userCache);
                 return result;
-            })
-            .catch(function () {
+            }).catch(function () {
                 throw new Error('Could not find user: ' + username);
             });
     }
@@ -109,10 +117,9 @@ function fn4() {
         mock(function () {
             resolve(10);
         });
-    })
-        .then(function (result) {
-            console.log('fn4 // Bo Derick?', result);
-        });
+    }).then(function (result) {
+        console.log('fn4 // Bo Derick?', result);
+    });
 }
 
 function fn5() {
@@ -120,37 +127,35 @@ function fn5() {
         mock(function () {
             resolve(2);
         });
-    })
-        .then(function (num) {
-            console.log('fn5 // take ' + num + ' then double it');
-            return num * 2;
-        })
-        .then(function (num) {
-            console.log('fn5 // then double it', num);
-            return num * 2;
-        })
-        .then(function (num) {
-            console.log('fn5 // and finally?', num);
-        });
+    }).then(function (num) {
+        console.log('fn5 // take ' + num + ' then double it');
+
+        return num * 2;
+    }).then(function (num) {
+        console.log('fn5 // then double it', num);
+
+        return num * 2;
+    }).then(function (num) {
+        console.log('fn5 // and finally?', num);
+    });
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 // CATCH
 function fn6() {
-
     new Promise(function (resolve, reject) {
         mock(function () {
             reject('Failure achieved!');
         });
-    })
-        .catch(function (e) {
-            console.log('fn6 // catch: ', e);
-        });
+    }).catch(function (err) {
+        console.log('fn6 // catch: ', err);
+    });
 }
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-fn1();
-fn2();
-fn3();
-fn4();
-fn5();
-fn6();
+relay(fn1, 1);
+relay(fn2, 2);
+relay(fn3, 3);
+relay(fn4, 4);
+relay(fn5, 5);
+relay(fn6, 6);
